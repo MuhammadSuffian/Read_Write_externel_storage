27d0bbb41ee328af4b14d2252acfd0fffa8b16e1
Muhammad Suffian Tafoor <112380672+MuhammadSuffian@users.noreply.github.com>
2024-12-31 10:03:29 +0500

    v1

--- app/src/main/java/com/example/read_write_externel_storage/MainActivity.java
+++ app/src/main/java/com/example/read_write_externel_storage/MainActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.read_write_externel_storage.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +26,33 @@ public class MainActivity extends AppCompatActivity {
             binding.etText.setText("");
         });
         binding.btnReadData.setOnClickListener(v->{
+            try {
 
+
+            }
+            catch (Exception e){
+
+            }
         });
         binding.btnWriteData.setOnClickListener(v->{
-
+            try {
+                String msg=binding.etText.getText().toString().trim();
+                if(msg.isEmpty()){
+                    binding.etText.setError("Message is Empty");
+                    binding.etText.requestFocus();
+                }
+                else{
+                    File directory=getExternalFilesDir(null);
+                    File file=new File(directory,"myfile.txt");
+                    FileOutputStream fout=new FileOutputStream(file);
+                    fout.write(msg.getBytes());
+                    fout.close();
+                    Snackbar.make(binding.btnReadData,"Message Write Successfuly",Snackbar.LENGTH_SHORT).show();
+                }
+            }
+            catch (Exception e){
+                Snackbar.make(binding.btnReadData,"Error:"+e,Snackbar.LENGTH_SHORT).show();
+            }
         });
     }
 }
